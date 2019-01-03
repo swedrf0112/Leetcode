395f5d10a41ce061674107a00d57f17a3fa26fbf
swedrf0112 <swedrf0112@gmail.com>
2019-01-04 00:17:12 +0800

    Update buddyStrings.java

--- 859_Buddy_Strings/buddyStrings.java
+++ 859_Buddy_Strings/buddyStrings.java
@@ -6,8 +6,8 @@ class buddyStrings{
 		
 		/*
 		(1) 兩個字串如果相等，字串裡面如果有重複的，可以交換位置相等 => return true
-		                      字串裡面不重複 => return false
-            => 直接開一個 array 紀錄字的出現次數，如果有一個字大於 1 代表可以 swap 之後變成一樣的 ex: aab, aab
+		                                              字串裡面不重複 => return false
+		    => 直接開一個 array 紀錄字的出現次數，如果有一個字大於 1 代表可以 swap 之後變成一樣的 ex: aab, aab
 			
 		(2) 兩個字串如果不相等，紀錄第一個不相等 與 第二個不相等的位置在哪，試試看可不可以 swap 
 		*/
