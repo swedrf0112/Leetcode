18081ee4582a170ec6c10b306cca2abc48af7450
swedrf0112 <swedrf0112@gmail.com>
2019-01-08 22:51:56 +0800

    Update fairCandySwap.py

--- 888_Fair_Candy_Swap/fairCandySwap.py
+++ 888_Fair_Candy_Swap/fairCandySwap.py
@@ -17,7 +17,7 @@ for i in range(len(A)):
 => 2A[i] - 2B[j] = sum(A) - sum(B)
 => B[j] = A[i] - (sum(A) - sum(B)) / 2
 
-所以, 只要遍歷A, 找出這個數字有沒有出現在B中即可
+所以, 只要遍歷A, 找出計算完的結果有沒有出現在B中即可
 '''
 
 sum_a, sum_b, set_b = sum(A), sum(B), set(B)
