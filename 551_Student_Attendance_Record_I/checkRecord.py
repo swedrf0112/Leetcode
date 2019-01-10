91e354c05607234c7ef98e7ebc2a77967381c482
swedrf0112 <swedrf0112@gmail.com>
2019-01-10 23:11:37 +0800

    Update checkRecord.py

--- 551_Student_Attendance_Record_I/checkRecord.py
+++ 551_Student_Attendance_Record_I/checkRecord.py
@@ -12,10 +12,10 @@ a_cnt = 0
 l_conti_cnt = 0
 
 for i in range(len(s)):
-	if s[i] == "L":
-		l_conti_cnt += 1
-		if l_conti_cnt > 2:
-			return False
+    if s[i] == "L":
+        l_conti_cnt += 1
+        if l_conti_cnt > 2:
+        return False
 
     else: 
         l_conti_cnt = 0 ## 如果字是"A"或"P", 連續的L count歸0
