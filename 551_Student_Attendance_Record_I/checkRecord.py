f5f334d34b53f3ca174cd00aa7fcee3661ce6794
swedrf0112 <swedrf0112@gmail.com>
2019-01-10 22:45:07 +0800

    Update checkRecord.py

--- 551_Student_Attendance_Record_I/checkRecord.py
+++ 551_Student_Attendance_Record_I/checkRecord.py
@@ -1,11 +1,22 @@
 ## https://leetcode.com/problems/student-attendance-record-i/
 
+'''
+
+'''
+
+## Method 1:
+return s.count("A") <= 1 and s.find("LLL") == -1
+
+## 方法二:
 a_cnt = 0
 l_max_conti_cnt = 0
 l_conti_cnt = 0
+
 for i in range(len(s)):
     if s[i] == "L":
         l_conti_cnt += 1
+        if i == len(s) - 1 and l_conti_cnt > l_max_conti_cnt:
+            l_max_conti_cnt = l_conti_cnt        
     else: 
         if s[i] == "A":
             a_cnt += 1
@@ -14,8 +25,5 @@ for i in range(len(s)):
             l_max_conti_cnt = l_conti_cnt
             
         l_conti_cnt = 0
-        
-if l_conti_cnt > l_max_conti_cnt:
-    l_max_conti_cnt = l_conti_cnt
 		
 return a_cnt <= 1 and l_max_conti_cnt <= 2
